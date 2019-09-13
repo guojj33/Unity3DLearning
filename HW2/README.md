93088cbfbbf44f5cb6fdd74492b506bccdeb161f
guojj33 <guojj33@mail2.sysu.edu.cn>
2019-09-13 15:28:54 +0800

    update

---
+++ HW2/README.md
@@ -0,0 +1,23 @@
+## TicTacToe 井字棋
+
+### 演示视频
+
+<a href="" target="_blank">视频</a>
+
+### 环境
+* Unity 5.5.0f3(64bit)
+* win10
+
+### 运行方法
+1. 直接双击TicTacToe/Assets/scene.unity
+2. 或者通过Unity3D查找到TicTacToe工程文件夹打开
+
+### 游戏说明
+* 游戏中会提示轮到哪一方
+* 游戏可能达成平手或某一方获胜，此时需要点击Restart才能重新开始游戏
+
+### 运行结果
+* #### 一方获胜  
+![](assets/game.PNG)
+* #### 平手  
+![](assets/tie.jpg)
---
+++ HW2/TicTacToe/Assets/TicTacToe.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicTacToe : MonoBehaviour {
+
+	private int turn = 1;
+	private int[,] state = new int[3,3];
+	int leftMost = 260;
+	int topMost = 140;
+	int buttonWidth = 50;
+	int posLeft = 9;
+
+	// Use this for initialization
+	void Start () {
+		reset ();
+	}
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (leftMost, topMost + 3 * buttonWidth + 10, 60, 20), "Restart")) {
+			reset ();
+		}
+		int result = check ();
+		if (result != 0) {
+			string win = check () == 1 ? "X" : "O";
+			win += " wins!";
+			GUI.Label (new Rect (leftMost + 1 * buttonWidth + 40, topMost + 3 * buttonWidth + 10, 50, buttonWidth), win);
+		} else {
+			if (posLeft == 0) {
+				GUI.Label (new Rect (leftMost + 1 * buttonWidth + 40, topMost + 3 * buttonWidth + 10, 100, buttonWidth), "Tie!");
+			} else {
+				string whoseTurn = (turn == 1 ? "X" : "O");
+				whoseTurn += " 's turn.";
+				GUI.Label (new Rect (leftMost + 1 * buttonWidth + 40, topMost + 3 * buttonWidth + 10, 100, buttonWidth), whoseTurn);
+			}
+		}
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				if (state [i,j] == 1) {
+					GUI.Button (new Rect (leftMost + i * buttonWidth, topMost + j * buttonWidth, buttonWidth, buttonWidth), "X");
+				} else if (state [i,j] == -1) {
+					GUI.Button (new Rect (leftMost + i * buttonWidth, topMost + j * buttonWidth, buttonWidth, buttonWidth), "O");
+				} else if (GUI.Button (new Rect (leftMost + i * buttonWidth, topMost + j * buttonWidth, buttonWidth, buttonWidth), "")){
+					if (result == 0) {
+						state [i, j] = turn;
+						posLeft = (posLeft == 0) ? 0 : posLeft - 1;
+					}
+					turn = -turn;
+				}
+			}
+		}
+	}
+
+	void reset(){
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				state [i,j] = 0;
+			}
+		}
+		posLeft = 9;
+	}
+
+	int check(){
+		for (int i = 0; i < 3; ++i) {
+			if (state [0, i] == state [1, i] && state [0, i] == state [2, i] && state [1, i] == state [2, i]) {
+				return state [0, i];
+			}
+		}
+		for (int i = 0; i < 3; ++i) {
+			if (state [i, 0] == state [i, 1] && state [i, 0] == state [i, 2] && state [i, 1] == state [i, 2]) {
+				return state [i, 0];
+			}
+		}
+		if (state [0, 0] == state [1, 1] && state [0, 0] == state [2, 2] && state [1, 1] == state [2, 2]) {
+			return state [0, 0];
+		}
+		if (state [2, 0] == state [1, 1] && state [2, 0] == state [0, 2] && state [1, 1] == state [0, 2]) {
+			return state [2, 0];
+		}
+		return 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
